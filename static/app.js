14eb5667259d625d11fa258b4d7687c8687759aa
TheCaffeinatedOne <thecaffinatedone@protonmail.com>
2021-04-16 18:59:08 -0400

    yea

---
+++ static/app.js
@@ -0,0 +1,34 @@
+const socket = io()
+const alias = document.getElementById('alias')
+const chat = document.querySelector('.chat-form')
+const chatInput = document.querySelector('.chat-input')
+
+console.log(chatInput)
+
+var output1 = alias.value + ": " + chatInput.value
+var output = output1;
+
+console.log("debug:" + chatInput.value)
+
+chat.addEventListener('submit', e => {
+  e.preventDefault()
+  console.log(chatInput.value)
+  console.log(output1.value)
+  socket.emit('chat', alias.value + ": " + chatInput.value)
+  chatInput.value = ''
+})
+
+const chatDump = document.querySelector('.chat-dump')
+const render = ({message, id}) => {
+  const div = document.createElement('div')
+  div.classList.add('chat-message')
+  if (id === socket.id) { // broadcasted chat is from this client
+    div.classList.add('chat-message--user')
+  }
+  div.innerText = message // insert message into new div
+  chatDump.appendChild(div)
+}
+
+socket.on('chat', data => {
+  render(data)
+})
